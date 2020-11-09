baf6fdc5c53b0c51f9aa71689e8e93b24ea5afbf
Lauren Brown <laurenbrown108@gmail.com>
2020-11-09 11:50:48 -0500

    passport stuff.

--- server.js
+++ server.js
@@ -1,18 +1,84 @@
+// require('dotenv').config()
+// const express = require("express");
+// const PORT = process.env.PORT || 3001;
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const passport = require("passport");
+// const routes = require("./routes");
+
+// const app = express();
+
+
+// app.use(express.static("public"));
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// if(process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+// }
+
+// const db = require("./config/keys")
+
+// //MongoDB connection
+// mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/freshstart", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+
+// //Routes
+// // app.use(require("./routes"));
+// // Passport middleware
+// app.use(passport.initialize());
+// // Passport config
+// require("./config/passport")(passport);
+// // Routes
+// app.use(routes);
+
+// app.listen(PORT, () => {
+//     console.log(`App listening on http://localhost:${PORT}`)
+// })
+
+// Pulls in our required dependencies.
 const express = require("express");
-const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const routes = require("./routes");
+require("dotenv").config();
+
+// Initializes the application using the express dependency.
 const app = express();
 
-//Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Applies the middleware function.
+app.use(bodyParser.urlencoded({ extended: false }));
 
-if(process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-}
+  }
 
-//Routes
-app.use(require("./routes"));
+app.use(bodyParser.json());
+
+// Database configuration.
+const db = require("./config/keys");
+
+// MongoDB connection using the mongoose dependency.
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/freshstart", { useNewUrlParser: true, useUnifiedTopology: true }).then (() => console.log("Successfully made connection to MongoDB."))
+.catch(err => console.log(err));
+
+// Passport middleware.
+app.use(passport.initialize());
+
+// Passport configuration.
+require("./config/passport")(passport);
+
+// Routes
+app.use(routes);
+
+// Sets the port for the server to run on.
+const PORT = process.env.PORT || 3001;
+
 
 app.listen(PORT, () => {
     console.log(`App listening on http://localhost:${PORT}`)
 })
+
+
---
+++ validation/register.js
@@ -0,0 +1,28 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+
+// Convert empty fields to an empty string so we can use validator functions
+
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+
+// Email validation
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Please enter an email address.";
+  } else if (!Validator.isEmail(data.email)) {
+    errors.email = "Please enter a valid email address";
+  }
+// Password validation
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Please enter a password";
+  }
+
+return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
